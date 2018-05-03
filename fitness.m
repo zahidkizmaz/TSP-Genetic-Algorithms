a8d6778d8a35413cdc37c1a6dd497e6e38e98a0a
zahidkizmaz <mzkizmaz@hotmail.com>
2018-05-04 00:48:06 +0300

    fitness function is donegit add -A

---
+++ fitness.m
@@ -0,0 +1,19 @@
+function [ fitnessValues ] = fitness( distances, pop )
+%UNTÝTLED Summary of this function goes here
+%   Detailed explanation goes here
+    [popSize, col] = size(pop);
+    pathTotalDistances = zeros(popSize,1);
+    fitnessValues = zeros(popSize,1);
+    for i=1:popSize;
+       for j=1:col-1;
+          pathTotalDistances(i) = pathTotalDistances(i) + distances(pop(i,j),pop(i,j+1))
+       end 
+    end
+    minPath = min(pathTotalDistances);
+    maxPath = max(pathTotalDistances);
+   
+    for i=1:length(pathTotalDistances);
+        fitnessValues(i,1)=(maxPath - pathTotalDistances(i,1)+0.000001) / (maxPath-minPath+0.00000001);
+    end
+end
+
--- main.m
+++ main.m
@@ -1,11 +1,17 @@
+clear;
+clc;
 prompt = 'What is the number of cities in this TSP program? ';
 numberOfCities = input(prompt);
 
 prompt = 'What is the number of population size? ';
 popSize = input(prompt);
-
+tic
 cities = rand(2,numberOfCities) * 10;
 
 distances = calculateDistance(cities);
 
-population = population(numberOfCities, popSize);
+pop = population(numberOfCities, popSize);
+
+fitnessValues = fitness(distances, pop);
+
+timeElapsed = toc
