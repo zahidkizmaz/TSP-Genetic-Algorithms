afe9799a093fc894f9ed83152f697fcff57b51a5
zahidkizmaz <mzkizmaz@hotmail.com>
2018-05-04 13:51:44 +0300

    crossover.m added, it is uses order one crossover method.

---
+++ crossover.m
@@ -0,0 +1,30 @@
+function [ childPath ] = crossover( parent1Path, parent2Path )
+% Generating child path from given two parent pathes by using ordered one
+% crossover method.
+
+    [l, length] = size(parent1Path);
+    childPath = zeros(l,length);
+    setSize = int8(length/2) -1;
+    offset = randi(setSize);
+    
+    for i=offset:setSize+offset-1;
+        childPath(1,i) = parent1Path(1,i);
+    end
+    iterator = i+1;
+    j = iterator;
+    while any(childPath == 0);
+        if j > length;
+            j = 1;
+        end
+        
+        if iterator > length;
+            iterator = 1;
+        end
+        if ~any(childPath == parent2Path(1,j));
+            childPath(1,iterator) = parent2Path(1,j);
+           iterator = iterator + 1;
+        end
+        j = j + 1;
+    end
+end
+
